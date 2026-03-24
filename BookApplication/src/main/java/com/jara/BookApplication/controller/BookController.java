5951e58fd168759376f8aa03bcb93db2302e448d
codewith-rajbotre <rajbotre2@gmail.com>
2026-03-24 09:41:11 +0530

    BookApplication Backend in Spring Boot

--- BookApplication/src/main/java/com/jara/BookApplication/controller/BookController.java
+++ BookApplication/src/main/java/com/jara/BookApplication/controller/BookController.java
@@ -1,8 +1,14 @@
 package com.jara.BookApplication.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +27,25 @@ class BookController{
           Book newBook = service.addBook(book);
           return ResponseEntity.ok(newBook);
      }
+
+     @GetMapping("/showBooks")
+     public List<Book> getBooks(){
+         return service.getBooks();
+     }
+
+    @GetMapping("/getBookById/{book_id}")
+    public Book getBookById(@PathVariable int book_id){
+             return service.getBookById(book_id);
+    }
+
+    @PutMapping("/updateBook/{id}")
+    public Book updateBook(@PathVariable int id, @RequestBody Book book){
+        return service.updateBook(id, book);
+    }
+
+    @DeleteMapping("/deleteBook/{id}")
+    public void deleteBookById(@PathVariable int id){
+           service.deleteBookById(id);
+    }
 
 }
--- BookApplication/src/main/java/com/jara/BookApplication/service/BookService.java
+++ BookApplication/src/main/java/com/jara/BookApplication/service/BookService.java
@@ -1,5 +1,7 @@
 package com.jara.BookApplication.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,25 @@ public class BookService{
     public Book addBook(Book book){
          return repository.save(book);
     }
+
+    public List<Book> getBooks(){
+        return repository.findAll();
+    }
+
+    public Book getBookById(int id){
+        return repository.findById(id).orElse(null);
+    }
+
+   public Book updateBook(int id, Book book){
+         Book existingBook = repository.findById(id).orElse(null);
+         existingBook.setName(book.getName());
+         existingBook.setAuthor(book.getAuthor());
+         existingBook.setPublishedYear(book.getPublishedYear());
+         return repository.save(existingBook);
+   }
+
+   public void deleteBookById(int id){
+          repository.deleteById(id);
+   }
 
 }
